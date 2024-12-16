c326c68267482f5cd7bae0e8f99809cdb0a19896
arfiravy0 <arfiraandayani1000@gmail.com>
2024-12-16 08:55:06 +0700

    first commit

---
+++ resources/views/client/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © F R Y.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Uts Web Lanjut by <a href="#!" class="text-decoration-underline">Arfira Andayani</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
